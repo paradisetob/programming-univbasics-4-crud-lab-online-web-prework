d4b3a31e806cdbcd3a38cda95b8fa110b341c55e
tracey brimage <onelovenilynn@gmail.com>
2019-09-09 20:10:20 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,52 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+
+colors=['green', 'yellow','purple', 'pink']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+colors=['green', 'yellow','purple', 'pink']
+
+colors >>'blue
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+colors = ['green', 'yellow','purple', 'pink']
+colors.unshift ('')
 end
 
 def remove_element_from_end_of_array(array)
-  
+  colors=['green', 'yellow','purple', 'pink',]
+pink = colors.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+
+  colors=['green', 'yellow','purple', 'pink']
+green = colors.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    colors=['green', 'yellow','purple', 'pink']
+colors.index[pink]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    colors=['green', 'yellow','purple', 'pink']
+
+colors[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    colors=['green', 'yellow','purple', 'pink']
+colors[4]
 end
 
 def update_element_from_index(array, index_number, element)
-
+    colors=['green', 'yellow','purple', 'pink']
+colors[2]=purple
 end
